eddcaf139fd98c4baa23f9347fee816fda2d4f5b
pranavRBM <pranavbm05@gmail.com>
2024-06-23 20:15:55 +0530

    added navbar and some other functionality

--- paddyApp/settings.py
+++ paddyApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#y^362p^ib)ag$)fldk2u%x-bf_p+o$zqqs*ij=95(6!p277pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,15 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framework',
-    # 'corsheaders',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -73,8 +73,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'paddyApp.wsgi.application'
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
